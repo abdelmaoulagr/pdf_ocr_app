0861bb45f666f176c3bc844416ff82720301d6ba
Omarelkaouri <omar.elkaouri@edu.uiz.ac.ma>
2023-06-04 23:53:05 +0100

    last thing

--- frontend/src/AdminHome.tsx
+++ frontend/src/AdminHome.tsx
@@ -4,15 +4,24 @@ import AdminDrawer from "./components/AdminNav";
 import Test from "./components/test";
 import LoiCards from "./components/AdminOutput";
 import DataTest from "./components/data_test";
+import { useState } from "react";
+import { listOfSearch } from "./components/Search";
 
 function AdminHome() {
   var Loi = DataTest();
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleRefresh = (isLoaded: any) => {
+    if (isLoaded) {
+      setRefreshKey((prevKey) => prevKey + 1);
+    }
+  };
   return (
     <>
       <div className="All">
         <div className="nav">
           <div className="search">
-            <SearchBar></SearchBar>
+            <SearchBar handleRefresh={handleRefresh}></SearchBar>
           </div>
           <div className="Drawer">
             <AdminDrawer></AdminDrawer>
@@ -20,7 +29,11 @@ function AdminHome() {
         </div>
         <div className="main">
           <div className="child-1">
-            <LoiCards lois={Loi} />
+            {listOfSearch.length != 0 ? (
+              <LoiCards key={refreshKey} lois={listOfSearch} />
+            ) : (
+              <LoiCards key={refreshKey} lois={Loi} />
+            )}
           </div>
         </div>
       </div>
